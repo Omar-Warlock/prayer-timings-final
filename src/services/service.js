1f425ce84233c9e6c6740d13e33cea80d0b6e5cc
OMR <cartoon6676@gmail.com>
2026-04-03 15:25:13 +0200

    finished

--- src/services/service.js
+++ src/services/service.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export async function getPrayerTimingData(country, city) {
   return await axios.get(
-    `http://api.aladhan.com/v1/timingsByCity?country=${country}&city=${city}`,
+    `https://api.aladhan.com/v1/timingsByCity?country=${country}&city=${city}`,
   );
 }
